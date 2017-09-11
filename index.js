5c511513075ba5a73b0a5feaff7b3420fe42b4f7
jordaninthewind <jordan.kline@gmail.com>
2017-09-11 23:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function happyHolidayTo (holiday, name) {
 }
 
 function holidayCountdown (name, days) {
-  return "It'/'s 20 days until Mother'/'s Day!";
+  return "It's 20 days until Mother's Day!";
 }
